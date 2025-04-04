73c0d20e0f7af59fa92c8488220df476f2617b26
Dhanny Adhi Pramana <dhanny.pramana@nostratech.com>
2025-04-04 13:36:50 +0700

    chore(config): setup eslint and fix lint

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,28 +1,36 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
-  compatibilityDate: '2024-11-01',
-  devtools: { enabled: true },
+    compatibilityDate: '2024-11-01',
+    devtools: { enabled: true },
 
-  devServer: {
-    port: Number.parseInt(import.meta.env.NUXT_DEVELOPMENT_PORT ?? '8000', 10),
-  },
+    modules: ['@nuxt/eslint'],
 
-  vite: {
-    define: {
-        'import.meta.env.NUXT_MAIL_KEY': JSON.stringify(import.meta.env.NUXT_PUBLIC_MAIL_KEY),
-        'import.meta.env.NUXT_EMAIL_URL': JSON.stringify(import.meta.env.NUXT_PUBLIC_EMAIL_URL),
-        'import.meta.env.API_BASE_PATH': JSON.stringify(import.meta.env.NUXT_PUBLIC_API_BASE_PATH),
-        'import.meta.env.SITE_URL': JSON.stringify(import.meta.env.NUXT_PUBLIC_SITE_URL),
-        'import.meta.env.FIREBASE_API_ID': JSON.stringify(import.meta.env.NUXT_PUBLIC_FIREBASE_API_ID),
-        'import.meta.env.FIREBASE_AUTH_DOMAIN': JSON.stringify(import.meta.env.NUXT_PUBLIC_FIREBASE_AUTH_DOMAIN),
-        'import.meta.env.FIREBASE_PROJECT_ID': JSON.stringify(import.meta.env.NUXT_PUBLIC_FIREBASE_PROJECT_ID),
-        'import.meta.env.FIREBASE_STORAGE_BUCKET': JSON.stringify(import.meta.env.NUXT_PUBLIC_FIREBASE_STORAGE_BUCKET),
-        'import.meta.env.FIREBASE_MESSAGING_SENDER_ID': JSON.stringify(import.meta.env.NUXT_PUBLIC_FIREBASE_MESSAGING_SENDER_ID),
-        'import.meta.env.FIREBASE_APP_ID': JSON.stringify(import.meta.env.NUXT_PUBLIC_FIREBASE_APP_ID),
+    devServer: {
+        port: Number.parseInt(import.meta.env.NUXT_DEVELOPMENT_PORT ?? '8000', 10),
     },
-  },
 
-  typescript: {
-    typeCheck: true
-  },
-})
+    vite: {
+        define: {
+            'import.meta.env.NUXT_MAIL_KEY': JSON.stringify(import.meta.env.NUXT_PUBLIC_MAIL_KEY),
+            'import.meta.env.NUXT_EMAIL_URL': JSON.stringify(import.meta.env.NUXT_PUBLIC_EMAIL_URL),
+            'import.meta.env.API_BASE_PATH': JSON.stringify(import.meta.env.NUXT_PUBLIC_API_BASE_PATH),
+            'import.meta.env.SITE_URL': JSON.stringify(import.meta.env.NUXT_PUBLIC_SITE_URL),
+            'import.meta.env.FIREBASE_API_ID': JSON.stringify(import.meta.env.NUXT_PUBLIC_FIREBASE_API_ID),
+            'import.meta.env.FIREBASE_AUTH_DOMAIN': JSON.stringify(import.meta.env.NUXT_PUBLIC_FIREBASE_AUTH_DOMAIN),
+            'import.meta.env.FIREBASE_PROJECT_ID': JSON.stringify(import.meta.env.NUXT_PUBLIC_FIREBASE_PROJECT_ID),
+            'import.meta.env.FIREBASE_STORAGE_BUCKET': JSON.stringify(import.meta.env.NUXT_PUBLIC_FIREBASE_STORAGE_BUCKET),
+            'import.meta.env.FIREBASE_MESSAGING_SENDER_ID': JSON.stringify(import.meta.env.NUXT_PUBLIC_FIREBASE_MESSAGING_SENDER_ID),
+            'import.meta.env.FIREBASE_APP_ID': JSON.stringify(import.meta.env.NUXT_PUBLIC_FIREBASE_APP_ID),
+        },
+    },
+
+    typescript: {
+        typeCheck: true,
+    },
+
+    eslint: {
+        config: {
+            standalone: false,
+        },
+    },
+});
