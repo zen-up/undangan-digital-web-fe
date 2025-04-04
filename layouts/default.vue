21bd169c9a204c5600a8a7989ce6d4bb6e82d351
Dhanny Adhi Pramana <dhanny.pramana@nostratech.com>
2025-04-04 14:38:06 +0700

    feat(base): define base layouts

---
+++ layouts/default.vue
@@ -0,0 +1,11 @@
+<template>
+    <section class="min-h-dvh grid grid-rows-[auto_1fr_auto]">
+        <header />
+
+        <main>
+            <slot />
+        </main>
+
+        <footer />
+    </section>
+</template>
