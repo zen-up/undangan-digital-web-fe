5c9f2f986798068aa0239434f7d50ee547c94248
Dhanny Adhi Pramana <dhanny.pramana@nostratech.com>
2025-05-11 20:06:50 +0700

    chore(i18n): setup i18n with yaml translations

--- i18n/i18n.config.ts
+++ i18n/i18n.config.ts
@@ -1,12 +1,29 @@
-import en from '../locales/en.json';
-import id from '../locales/id.json';
+import yaml from 'js-yaml';
+
+const translations = import.meta.glob(
+    '../locales/*.yaml',
+    {
+        query: 'raw',
+        eager: true,
+        import: 'default',
+    },
+);
+
+const messages = Object
+    .entries(translations)
+    .reduce((acc, [path, content]) => {
+        const locale = path
+            .split('/')
+            .pop()
+            ?.replace('.yaml', '') || '';
+        acc[locale] = yaml
+            .load(String(content)) as Record<string, string>;
+        return acc;
+    }, {} as Record<string, Record<string, string>>);
 
 export default defineI18nConfig(() => ({
     legacy: false,
     locale: 'id',
     fallbackLocale: 'id',
-    messages: {
-        en,
-        id,
-    },
+    messages,
 }));
